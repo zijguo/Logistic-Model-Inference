a81657adbed409db62dd2639877beb6c449125f5
zijguo <zijguo@gmail.com>
2020-12-17 06:16:50 -0500

    Update Example_LF_logistic.R

--- Example_LF_logistic.R
+++ Example_LF_logistic.R
@@ -11,8 +11,8 @@ for(i in 1:p){
   }
   A1
 }
-n = 100
-p = 400
+n = 400
+p = 500
 mu <- rep(0,p)
 rho = 0.5
 Cov <- (A1gen(rho,p))/2
@@ -25,8 +25,9 @@ for(i in 1:p){
 beta <- rep(0,p)
 beta[1:10] <- 0.5*c(1:10)/10
 a0 = 0
-set.seed(12)
+set.seed(2020)
 loading <- MASS::mvrnorm(1,mu,Cov2)
+loading[20:p]<-loading[20:p]/5
 true <- expo(t(loading)%*%beta+a0)
 X <- MASS::mvrnorm(n,mu,Cov)
 exp_val <- X%*%beta+a0
@@ -34,3 +35,9 @@ prob <- exp(exp_val)/(1+exp(exp_val))
 y <- rbinom(n,1,prob)
 Est <- LF_logistic(X = X, y = y, loading = loading, intercept = TRUE, weight = NULL)
 
+### outputs of LF_logistic
+### Est$prop.est: the bias-corrected estimator of the case probability
+### Est$CI: confidence intervals for the case probability 
+### Est$proj: the p-dim projection direction for bias correction
+### Est$plug.in: the plug-in estimator of the case probability
+### Est$case: 1 denotes that this observation is labelled as case (case probability is above 0.5); 0 denotes control (case probability is below 0.5)
